609c3bcd67ca2fd5b56526612992bbe9cbe0e27a
Wxqxsys <wxqxsys@gmail.com>
2026-03-24 19:36:42 +0300

    add closures

---
+++ Closures/tasks/createPrinters.js
@@ -0,0 +1,7 @@
+let list = [];
+
+for (var a = 0; a < 3; a++) {
+    list.push(a);
+}
+
+console.log(list);
---
+++ Closures/tasks/inBetween.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+function inBetween(a, b) {
+    return function(value) {
+        return a <= value && b >= value ? true : false;
+    }
+}
+
+console.log(arr.filter(inBetween(3, 6))); // [3, 4, 5, 6]
